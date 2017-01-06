deada38dbc9044e10a31091772763b5fb1d2cde9
valdiss <valentin.dissais@gmail.com>
2017-01-06 15:37:43 +0100

    ajout du dernier elève dans le dernier groupe choisit

--- app.js
+++ app.js
@@ -4,30 +4,38 @@ let eleves = ["Sophie", "Alexandra", "Alice", "Camille", "César", "Elise", "Fel
     "Ryhad", "Samuel", "Seifeddine", "Sylvain", "Valentin"
 ];
 
-let div = document.getElementById('picked');
+let ul = document.getElementById('picked');
 let button = document.getElementById('btn');
 let nbr = document.getElementById('nbr');
-let pick= [];
+let pick = [];
 let random = 0;
 
-button.addEventListener("click", function(){
-  if (!nbr.value){
-    alert ("Merci d'entrer un nombre d'élèves par groupes!");
-  }
-  else {
-    while (eleves.length >= nbr.value) {
-        pick = [];
-        for (let i = 0; i < nbr.value; i++) {
-            random = Math.floor(Math.random()* (eleves.length));
-            pick.push(eleves[random]);
-            eleves.splice(random, 1);
-        }
-        let p = document.createElement("p");
-        p.innerHTML = pick;
-        div.appendChild(p);
+button.addEventListener("click", function() {
+
+    if (!nbr.value) {
+        alert("Merci d'entrer un nombre d'élèves par groupes!");
     }
-    let p = document.createElement("p");
-    p.innerHTML = eleves;
-    div.appendChild(p);
-  }
+
+    else {
+        let j = 1;
+        while (eleves.length >= nbr.value) {
+
+            pick = [];
+            for (let i = 0; i < nbr.value; i++) {
+                random = Math.floor(Math.random() * (eleves.length));
+                pick.push(eleves[random]);
+                eleves.splice(random, 1);
+            }
+
+            let li = document.createElement("li");
+            li.innerHTML = "Groupe n°" + j + ": " + pick.join(', ');
+            ul.appendChild(li);
+            j++;
+        }
+        if(eleves.length > 0){
+          let last = document.querySelector("li:last-child");
+          last.innerHTML += ", " + eleves.join(', ');
+          ul.style.border = '1px solid black';
+        }
+    }
 });

b5ad60a7c9d071a1eb2a3435129ff0ee01eeb311
valdiss <valentin.dissais@gmail.com>
2017-01-08 17:49:06 +0100

    tirage aléatoire avec contraintes, v1

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-let eleves = ["Sophie", "Alexandra", "Alice", "Camille", "César", "Elise", "Felix", "Fodil",
-    "Ibni-Yamine", "Jamil", "Jean-Jacques", "Jessica", "Jonathan",
-    "Julien", "Kévin", "Laurène", "Linda", "Mario", "Mohamed", "Okba",
-    "Ryhad", "Samuel", "Seifeddine", "Sylvain", "Valentin"
-];
+let eleves = [];
+let vener = ["Alice", "Jessica"];
+// let venertwo = ["Okba","Alexandra"];
 let body = document.querySelector("body");
 let form = document.querySelector("form");
 let button = document.getElementById('btn');
@@ -18,7 +16,9 @@ form.addEventListener("submit", function() {
         "Julien", "Kévin", "Laurène", "Linda", "Mario", "Mohamed", "Okba",
         "Ryhad", "Samuel", "Seifeddine", "Sylvain", "Valentin"
     ];
-
+    if (nbr.value == 7) {
+        nbr.value = 8;
+    }
 
     if (!nbr.value) {
         alert("Merci d'entrer un nombre d'élèves par groupes!");
@@ -27,6 +27,7 @@ form.addEventListener("submit", function() {
         button.innerHTML = "Nouveau tirage";
 
         if (document.querySelector('ul')) {
+            sessionStorage.setItem('precedent', document.querySelector('ul'));
             document.querySelector('ul').remove();
         }
 
@@ -39,8 +40,22 @@ form.addEventListener("submit", function() {
             pick = [];
             for (let i = 0; i < nbr.value; i++) {
                 random = Math.floor(Math.random() * (eleves.length));
-                pick.push(eleves[random]);
-                eleves.splice(random, 1);
+
+                if ((eleves[random] == vener[0] && pick.includes(vener[1])) || (eleves[random] == vener[1] && pick.includes(vener[0]))) {
+
+                    alert("Alerte! 3ème Guerre Mondiale en approche! Refaites un tirage pour le bien de l'humanité!");
+                    ul.remove();
+
+                }
+                // else if ((eleves[random] == venertwo[0] && pick.includes(venertwo[1])) || (eleves[random] == venertwo[1] && pick.includes(venertwo[0]))) {
+                //     alert("oh oh Okba et Alexandra ensemble!");
+                //     window.location.reload();
+                // }
+                else {
+                    pick.push(eleves[random]);
+                    eleves.splice(random, 1);
+                }
+
             }
 
             let li = document.createElement("li");
@@ -55,10 +70,13 @@ form.addEventListener("submit", function() {
             }
 
         }
-        if (eleves.length > 0) {
+        if (eleves.length == 1) {
             let last = document.querySelector("li:last-child");
             last.innerHTML += ", " + eleves.join(', ');
+        } else if (eleves.length > 1) {
+            let li = document.createElement("li");
+            li.innerHTML = "Groupe n°" + j + ": " + eleves.join(', ');
+            ul.appendChild(li);
         }
     }
-    nbr.value = "";
 });

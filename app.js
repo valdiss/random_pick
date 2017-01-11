9537eff231f05a05e2023f9f5916d4154bdd6bda
valdiss <valentin.dissais@gmail.com>
2017-01-11 17:51:25 +0100

    ajouts des couple contraires

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 let eleves = [];
-let vener = ["Alice", "Jessica"];
-// let venertwo = ["Okba","Alexandra"];
+let contraire1 = ["Samuel","Kévin"];
+let contraire2 = ["Fodil","Sophie"];
+let contraire3 = ["Jean-Jacques","Okba"];
+let contraire4 = ["Laurène","Elise"];
+let contraire5 = ["Ryhad","Mario"];
+let contraire6 = ["Ibni-Yamine","Seifeddine"];
+let contraire7 = ["Valentin","Jamil"];
+let contraire8 = ["Alice","Jonathan"];
+let contraire9 = ["Camille","Julien"];
+let contraire10 = ["Jessica","Linda"];
+let contraire11 = ["César","Sylvain"];
+let contraire12 = ["Mohamed","Félix"];
 let body = document.querySelector("body");
 let form = document.querySelector("form");
 let button = document.getElementById('btn');
 let nbr = document.getElementById('nbr');
+let last = document.querySelector("li:last-child");
 let pick = [];
 let random = 0;
+let randomContraire = 0;
 
 form.addEventListener("submit", function() {
     event.preventDefault();
 
-    eleves = ["Sophie", "Alexandra", "Alice", "Camille", "César", "Elise", "Felix", "Fodil",
-        "Ibni-Yamine", "Jamil", "Jean-Jacques", "Jessica", "Jonathan",
-        "Julien", "Kévin", "Laurène", "Linda", "Mario", "Mohamed", "Okba",
-        "Ryhad", "Samuel", "Seifeddine", "Sylvain", "Valentin"
-    ];
+    eleves = ["Alexandra", "Alice", "Camille", "César", "Elise", "Félix", "Fodil", "Ibni-Yamine", "Jamil", "Jean-Jacques", "Jessica", "Jonathan", "Julien", "Kévin", "Laurène", "Linda", "Mario", "Mohamed", "Okba", "Ryhad", "Samuel", "Seifeddine", "Sophie", "Sylvain", "Valentin"];
     if (nbr.value == 7) {
         nbr.value = 8;
     }
@@ -27,7 +35,7 @@ form.addEventListener("submit", function() {
         button.innerHTML = "Nouveau tirage";
 
         if (document.querySelector('ul')) {
-            sessionStorage.setItem('precedent', document.querySelector('ul'));
+            // sessionStorage.setItem('precedent', document.querySelector('ul'));
             document.querySelector('ul').remove();
         }
 
@@ -40,23 +48,47 @@ form.addEventListener("submit", function() {
             pick = [];
             for (let i = 0; i < nbr.value; i++) {
                 random = Math.floor(Math.random() * (eleves.length));
-
-                if ((eleves[random] == vener[0] && pick.includes(vener[1])) || (eleves[random] == vener[1] && pick.includes(vener[0]))) {
-
-                    alert("Alerte! 3ème Guerre Mondiale en approche! Refaites un tirage pour le bien de l'humanité!");
-                    i--;
-
+                if ((i == 0 && eleves.length == 25)) {
+                    randomContraire = Math.floor(Math.random() * 2);
+                    pick.push(eleves[eleves.indexOf(contraire1[randomContraire])]);
+                    eleves.splice(eleves.indexOf(contraire1[randomContraire]), 1);
                 }
-                // else if ((eleves[random] == venertwo[0] && pick.includes(venertwo[1])) || (eleves[random] == venertwo[1] && pick.includes(venertwo[0]))) {
-                //     alert("oh oh Okba et Alexandra ensemble!");
-                //     window.location.reload();
-                // }
+
+                else if (eleves.length == (25-nbr.value)) {
+                    randomContraire = Math.floor(Math.random() * 2);
+                    pick.push(eleves[eleves.indexOf(contraire2[randomContraire])]);
+                    eleves.splice(eleves.indexOf(contraire2[randomContraire]), 1);
+                }
+
+                else if (eleves.length == (25-(nbr.value*2))) {
+                    randomContraire = Math.floor(Math.random() * 2);
+                    pick.push(eleves[eleves.indexOf(contraire3[randomContraire])]);
+                    eleves.splice(eleves.indexOf(contraire3[randomContraire]), 1);
+                }
+
+                else if ((eleves[random] == contraire1[1] && pick.includes(contraire1[0])) || (eleves[random] == contraire1[0] && pick.includes(contraire1[1]))) {
+
+                    alert("Alerte! 3ème Guerre Mondiale en approche! J'ai relancé un tirage pour le bien de l'humanité!");
+                    i--;
+                }
+                else if ((eleves[random] == contraire2[1] && pick.includes(contraire2[0])) || (eleves[random] == contraire2[0] && pick.includes(contraire2[1]))) {
+
+                    alert("Alerte! 3ème Guerre Mondiale en approche! J'ai relancé un tirage pour le bien de l'humanité!");
+                    i--;
+                }
+                else if ((eleves[random] == contraire3[1] && pick.includes(contraire3[0])) || (eleves[random] == contraire3[0] && pick.includes(contraire3[1]))) {
+
+                    alert("Alerte! 3ème Guerre Mondiale en approche! J'ai relancé un tirage pour le bien de l'humanité!");
+                    i--;
+                }
+
                 else {
                     pick.push(eleves[random]);
                     eleves.splice(random, 1);
                 }
 
             }
+
 
             let li = document.createElement("li");
             if (nbr.value == 1) {
@@ -69,14 +101,12 @@ form.addEventListener("submit", function() {
                 j++;
             }
 
+
+
         }
         if (eleves.length == 1) {
-            let last = document.querySelector("li:last-child");
+            last = document.querySelector("li:last-child");
             last.innerHTML += ", " + eleves.join(', ');
-        } else if (eleves.length > 1) {
-            let li = document.createElement("li");
-            li.innerHTML = "Groupe n°" + j + ": " + eleves.join(', ');
-            ul.appendChild(li);
         }
     }
 });

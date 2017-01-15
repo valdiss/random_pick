740b6651231416820df67c4514306189d697d626
valdiss <valentin.dissais@gmail.com>
2017-01-15 18:39:45 +0100

    dernier essai app2.js echec

--- js/app2.js
+++ js/app2.js
@@ -2,8 +2,7 @@ let elevespb = [];
 let randomgr = 0;
 let randomel = 0;
 let nombre = 0;
-let ligne1 = [];
-let ligne2 = [];
+let ligne = [];
 let body = document.querySelector("body");
 let form = document.querySelector("form");
 let nbr = document.getElementById('nbr');
@@ -34,65 +33,68 @@ form.addEventListener("submit", function() {
     p.innerHTML = "";
     alert.appendChild(p);
 
-    if (nombre == 7) { //----------------------------------------------Le chiffre 7 est problématique, sur 24 élèves, nous aurions 3 groupes de 7 et un de 3
-        nombre = 6; //----------------------------------------------la variable est juste là pour que l'utilisateur ne crois pas à un bug
+    // if (nombre == 7) { //----------------------------------------------Le chiffre 7 est problématique, sur 24 élèves, nous aurions 3 groupes de 7 et un de 3
+    //     nombre = 6; //----------------------------------------------la variable est juste là pour que l'utilisateur ne crois pas à un bug
+    // } else if (nombre == 11 || nombre == 10) { //----------------------------------------------Le chiffre 7 est problématique, sur 24 élèves, nous aurions 3 groupes de 7 et un de 3
+    //     nombre = 12; //----------------------------------------------la variable est juste là pour que l'utilisateur ne crois pas à un bug
+    // } else if (nombre == 9) { //----------------------------------------------Le chiffre 7 est problématique, sur 24 élèves, nous aurions 3 groupes de 7 et un de 3
+    //     nombre = 8; //----------------------------------------------la variable est juste là pour que l'utilisateur ne crois pas à un bug
+    // }
+
+    button.innerHTML = "Nouveau tirage";
+
+    if (document.querySelector('ul')) {
+        document.querySelector('ul').remove();
     }
 
-    else if (nombre == 5) { //----------------------------------------------Le chiffre 7 est problématique, sur 24 élèves, nous aurions 3 groupes de 7 et un de 3
-        nombre = 6; //----------------------------------------------la variable est juste là pour que l'utilisateur ne crois pas à un bug
-    }
+    p.innerHTML = "";
 
-    else if (nombre == 11 || nombre == 10) { //----------------------------------------------Le chiffre 7 est problématique, sur 24 élèves, nous aurions 3 groupes de 7 et un de 3
-        nombre = 12; //----------------------------------------------la variable est juste là pour que l'utilisateur ne crois pas à un bug
-    }
-    else if (nombre == 9) { //----------------------------------------------Le chiffre 7 est problématique, sur 24 élèves, nous aurions 3 groupes de 7 et un de 3
-        nombre = 8; //----------------------------------------------la variable est juste là pour que l'utilisateur ne crois pas à un bug
-    }
+    let ul = document.createElement("ul");
+    body.appendChild(ul);
+    let j = 1;
 
-    if (!nbr.value) {
-        p.innerHTML = "Merci d'entrer un nombre d'élèves";
-
-    } else {
-        button.innerHTML = "Nouveau tirage";
-
-        if (document.querySelector('ul')) {
-            document.querySelector('ul').remove();
+    for (let i = 0; i < Math.floor(elevespb.length/nbr.value); i++) {
+        console.log('premier loop');
+        if (elevespb.length < nbr.value) {
+          nombre = elevespb.length;
         }
+        for (let k = 0; k < nombre; k++) {
 
-        p.innerHTML = "";
-
-        let ul = document.createElement("ul");
-        body.appendChild(ul);
-        let j = 1;
-
-        while (elevespb.length > 0) {
-            if (elevespb.length<nbr.value) {
-              nombre = elevespb.length;
+            randomgr = Math.floor(Math.random() * elevespb.length);
+            randomel = Math.floor(Math.random() * 2);
+            if(elevespb[randomgr].length === 1){
+              k--;
+            }
+            else {
+              ligne.push(elevespb[randomgr][randomel]);
+              elevespb[randomgr].splice(randomel, 1);
             }
 
-            for (let i = 0; i < nombre; i++) {
-
-                randomgr = Math.floor(Math.random() * elevespb.length);
-                randomel = Math.floor(Math.random() * 2);
-                console.log ((elevespb.length) + " le groupe random n° " +randomgr );
-
-                ligne1.push(elevespb[randomgr][randomel]);
-                elevespb[randomgr].splice(randomel, 1);
-                ligne2.push(elevespb[randomgr][0]);
-                elevespb.splice(randomgr, 1);
-
-            }
-            var li = document.createElement("li");
-            li.innerHTML = "Groupe n°" + j + ": " + ligne1.join(', ');
-            ul.appendChild(li);
-            j++;
-            ligne1 = [];
-
-            var li = document.createElement("li");
-            li.innerHTML = "Groupe n°" + j + ": " + ligne2.join(', ');
-            ul.appendChild(li);
-            j++;
-            ligne2 = [];
         }
+        let li = document.createElement("li");
+        li.innerHTML = "Groupe n°" + j + ": " + ligne.join(', ');
+        ul.appendChild(li);
+        j++;
+        ligne = [];
     }
+    nombre = nbr.value;
+    while (elevespb.length) {
+       if (elevespb.length < nbr.value) {
+           nombre = elevespb.length;
+       }
+       for (let m = 0; m < nombre; m++) {
+
+           randomgr = Math.floor(Math.random() * elevespb.length);
+
+             ligne.push(elevespb[randomgr]);
+             elevespb.splice(randomgr, 1);
+
+       }
+       let li = document.createElement("li");
+       li.innerHTML = "Groupe n°" + j + ": " + ligne.join(', ');
+       ul.appendChild(li);
+       j++;
+       ligne = [];
+    }
+
 });
